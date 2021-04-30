d5b272b6ac5202c6f379fd02d880730a54a471ad
Tamoghna Pattanayak <42137018+tamoghnaaa2012@users.noreply.github.com>
2021-04-30 15:48:20 +0530

    Add files via upload

---
+++ Code/Mono-atomic.py
@@ -0,0 +1,44 @@
+#For mono-atomic
+
+
+
+import numpy as np
+import matplotlib.pyplot as plt
+import math
+#Creating the matrix
+E0 = -10
+Beta = -2
+n = 6000#Rank of matrix
+M = np.zeros((n,n))
+#print(M)
+for i in range (n):
+    for j in range (n):
+        if i == j:
+            M[i][j] = E0
+        elif j == i+1 :
+            M[i][j] = Beta
+        elif j == i-1 :
+            M[i][j] = Beta
+print(M) #Starting matrix is constructed.
+#Now our goal is to diagonalize the matrix.
+w,v = np.linalg.eig(M) #Eigen values of matrix M
+print(w)
+q = w.sort() #Arranged in ascending order
+#print(w)
+
+
+
+wid=math.sqrt(n)
+div=int(wid)
+
+
+plt.hist(w,bins= div)
+#plt.hist(w,bins = bi)
+d = w[n-1] - w[0]
+step = d/n
+print(step)
+xlegend = plt.xlabel('Eigen value')
+ylegend = plt.ylabel('Density of eigen states')
+#plt.style.use('ggplot')
+plt.title(f'For dimension n={n}')
+plt.show()
